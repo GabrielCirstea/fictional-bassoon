b72149821504283cc49cd1fe7bb3d33bae6cbf45
Gabriel Cirstea <gabriel_cirstea@protonmail.com>
2022-06-02 22:09:41 +0300

    Dummy round roby

--- start.py
+++ start.py
@@ -1,26 +1,25 @@
 import threading
 import time
 
-def handy(n: int, share):
-    print("share:", share)
-    time.sleep(n)
-    print("share:", share)
-
-def worker(jobs: list):
-    for j in jobs:
-        print(threading.get_ident(), "Worcking on", j.name)
-        time.sleep(j.duration)
-
 class Job:
     def __init__(self, name: str, duration: int):
         self.name = name
         self.duration = duration
+        self.remaining = duration
 
-# MAKE A GOT DAMN CLASS FOR WORKERS WITH THEYR SHARED JOBS LIST
+    def is_done(self):
+        if self.remaining <= 0:
+            return True
+        return False
+
+
 class Worker:
-    def __init__(self, jobs):
-        self.thread = threading.Thread(target=worker, args=(jobs,))
+    def __init__(self, jobs, name = "no_name"):
+        self.thread = threading.Thread(target=self.work, args=())
+        self.name = name
         self.jobs = jobs
+        self.running = True
+        self.quantum = 2
 
     def start(self):
         self.thread.start()
@@ -28,7 +27,7 @@ class Worker:
     def join(self):
         self.thread.join()
 
-    def tid(self):
+    def get_tid(self):
         return self.thread.native_id
 
     def is_working(self):
@@ -36,22 +35,39 @@ class Worker:
             return True
         return False
 
+    def stop(self):
+        self.running = False
+
+    # to be run in a thread
+    def work(self):
+        # optimize it with sime signals stuff
+        while self.running:
+            for j in self.jobs:
+                print(self.name, "Worcking on", j.name)
+                # round robin stuff
+                slp_time = min(j.duration, self.quantum)
+                time.sleep(slp_time)
+                j.remaining -= slp_time
+                if j.is_done():
+                    self.jobs.remove(j)
+        print("stoping worker:", self.name)
+
+
 def balance_baby(workers: Worker, target: Worker):
     for w in workers:
         if w == target:
             print("Hit the same")
             continue
-        if not w.is_working():
+        if w.is_working() and len(w.jobs) > 1:
             target.jobs.append(w.jobs[-1])
             w.jobs.pop(-1)
-            target.start()
             break
 
 def main():
-    jobs1 = [Job("a1",23),Job("a2",5),Job("a3",4), Job("a4",10)]
-    jobs2 = [Job("b1",6),Job("b2",3),Job("b3",2)]
-    t1 = Worker(jobs1)
-    t2 = Worker(jobs2)
+    jobs1 = [Job("a1",10),Job("a2",5),Job("a3",4), Job("a4",10)]
+    jobs2 = [Job("b1",3),Job("b2",3),Job("b3",2)]
+    t1 = Worker(jobs1, "worker1")
+    t2 = Worker(jobs2, "worker2")
   
     # starting thread 1
     t1.start()
@@ -64,14 +80,16 @@ def main():
         to_pop = []
         for (i, t) in enumerate(t_list):
             # first try to give something else to work on
-            if t.thread.is_alive() == False:
+            if t.is_working() == False:
                 balance_baby(t_list, t)
-            if t.thread.is_alive() == False:
+            if t.is_working() == False:
                 print("joining {}".format( t.thread.native_id))
+                t.stop()
                 t.join()
                 to_pop.append(i)
         for i in to_pop:
-            t_list.pop(i)
+            if i < len(t_list):
+                t_list.pop(i)
 
 if __name__ == "__main__":
     main()
